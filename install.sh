5acfbedacce9d59818f454a57465e7357a45a474
hieplpvip <baohiep2013@gmail.com>
2018-08-23 23:02:37 +0700

    Update scripts

--- install.sh
+++ install.sh
@@ -4,5 +4,5 @@ sudo sh build.sh
 sudo sh mount_efi.sh
 sudo rm -rf /Volumes/EFI/EFI/CLOVER/kexts/Other/AsusFnKeys.kext
 sudo cp -a ./build/Debug/AsusFnKeys.kext /Volumes/EFI/EFI/CLOVER/kexts/Other
-sudo sh install_daemon.sh
+sudo sh ./build/Debug/install_daemon.sh
 echo "Installed to EFI"
--- install_daemon.sh
+++ install_daemon.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 launchctl unload /Library/LaunchDaemons/com.hieplpvip.AsusFnKeysDaemon.plist 2>/dev/null
-sudo cp ./build/Debug/com.hieplpvip.AsusFnKeysDaemon.plist /Library/LaunchDaemons
-sudo cp ./build/Debug/AsusFnKeysDaemon /usr/bin
+sudo cp $DIR/com.hieplpvip.AsusFnKeysDaemon.plist /Library/LaunchDaemons
+sudo cp $DIR/AsusFnKeysDaemon /usr/bin
 launchctl load /Library/LaunchDaemons/com.hieplpvip.AsusFnKeysDaemon.plist
