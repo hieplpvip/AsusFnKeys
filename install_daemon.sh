941f6f016cfcaced13883858fccaf78ef2cc84e1
hieplpvip <baohiep2013@gmail.com>
2018-08-24 21:53:23 +0700

    Fix daemon not running at boot

--- install_daemon.sh
+++ install_daemon.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-launchctl unload /Library/LaunchDaemons/com.hieplpvip.AsusFnKeysDaemon.plist 2>/dev/null
-sudo cp $DIR/com.hieplpvip.AsusFnKeysDaemon.plist /Library/LaunchDaemons
+
+launchctl unload /Library/LaunchAgents/com.hieplpvip.AsusFnKeysDaemon.plist 2>/dev/null
+
 sudo cp $DIR/AsusFnKeysDaemon /usr/bin
-launchctl load /Library/LaunchDaemons/com.hieplpvip.AsusFnKeysDaemon.plist
+sudo chmod 755 /usr/bin/AsusFnKeysDaemon
+sudo chown root:wheel /usr/bin/AsusFnKeysDaemon
+
+sudo cp $DIR/com.hieplpvip.AsusFnKeysDaemon.plist /Library/LaunchAgents
+sudo chmod 644 /Library/LaunchAgents/com.hieplpvip.AsusFnKeysDaemon.plist
+sudo chown root:wheel /Library/LaunchAgents/com.hieplpvip.AsusFnKeysDaemon.plist
+
+launchctl load /Library/LaunchAgents/com.hieplpvip.AsusFnKeysDaemon.plist
